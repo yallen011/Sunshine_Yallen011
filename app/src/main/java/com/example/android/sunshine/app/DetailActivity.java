b8501848cac27544322730c86dbd2d72fdb39dd6
Yvonne <princess0115@gmail.com>
2015-12-28 11:11:25 -0500

    removed files that were added when I merged with origin

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -16,49 +16,30 @@
 
 package com.example.android.sunshine.app;
 
+import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.ShareActionProvider;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-
 public class DetailActivity extends ActionBarActivity {
-    private final static String LOG_TAG = DetailActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-
         if (savedInstanceState == null) {
-            // Create the detail fragment and add it to the activity
-            // using a fragment transaction.
-
-            Bundle arguments = new Bundle();
-            arguments.putParcelable(DetailFragment.DETAIL_URI, getIntent().getData());
-
-            DetailFragment fragment = new DetailFragment();
-            fragment.setArguments(arguments);
-
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.weather_detail_container, fragment);
-
-           /* getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new DetailFragment())
-                    .commit();*/
+                    .commit();
         }
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -75,7 +56,6 @@ public class DetailActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
@@ -89,13 +69,7 @@ public class DetailActivity extends ActionBarActivity {
      */
     public static class DetailFragment extends Fragment {
 
-        private final static String LOG_TAG = DetailFragment.class.getSimpleName();
-        private static final String FORECAST_SHARE_HASHTAG ="#SunshineApp";
-        private String mForecastStr;
         public DetailFragment() {
-
-            //set the menu to the fragment to let the fragment know that it has a menu.
-            setHasOptionsMenu(true);
         }
 
         @Override
@@ -104,57 +78,15 @@ public class DetailActivity extends ActionBarActivity {
 
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
-            //the detail Activity called via intent. Inspect the intent
+            // The detail Activity called via intent.  Inspect the intent for forecast data.
             Intent intent = getActivity().getIntent();
-            if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
-                Log.i(LOG_TAG, "Forecast data sent from forecast list: " + mForecastStr);
-                //Display forecast details as text
-                ((TextView) rootView.findViewById(R.id.detailsTextView)).setText(mForecastStr);
+            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.detailsTextView))
+                        .setText(forecastStr);
             }
+
             return rootView;
         }
-
-        @Override
-        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-
-            //inflate the menu; this adds items to the action bar if it is present.
-           inflater.inflate(R.menu.detailfragment, menu);
-
-            //retrieve the share menu item
-            MenuItem menuItem = menu.findItem(R.id.action_share);
-
-            // Getting the action provider associated with the menu item whose id is action_share
-            ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
-
-            // Attach an intent to this ShareActionProvider.  You can update this at any time,
-            // like when the user selects a new piece of data they might like to share.
-            if(mShareActionProvider != null){
-                mShareActionProvider.setShareIntent(createShareForecastIntent());
-            }else{
-                Log.i(LOG_TAG, "Share Action Provider is null?");
-            }
-
-        }
-
-        private Intent createShareForecastIntent() {
-
-            //ACTION_SEND is the android share intent for sharing content
-            Intent shareIntent = new Intent(Intent.ACTION_SEND);
-
-            //flag to let android now to stay inside this app when the back button is pressed after sharing;
-            //if this flag is omitted you would end up in the app that you shared the content to.
-            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-
-            //let android know we will be sending plain text
-            shareIntent.setType("text/plain");
-
-            //data to send in the intent, which is the forecast string plus the hashtag
-            shareIntent.putExtra(Intent.EXTRA_TEXT,
-                    mForecastStr +" " + FORECAST_SHARE_HASHTAG);
-
-            return shareIntent;
-        }
-
     }
 }
